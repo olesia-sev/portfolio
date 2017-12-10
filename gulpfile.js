a8079160a8c38a8784ad144698be3d7acedfe6ba
Olesia <olesya0191@gmail.com>
2017-12-10 14:01:00 +0300

    gulp, webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,97 @@
 const gulp = require('gulp');
 const pug = require('gulp-pug');
 
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+
+const gulpWebpack = require('gulp-webpack');
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.js');
+
 const paths = {
   root: './build',
   templates: {
     pages: 'src/templates/pages/*.pug',
     src: 'src/templates/**/*.pug',
-    dest: 'build/assets/'
+    //dest: 'build/assets/'
+  },
+  styles: {
+    src: 'src/styles/**/*.scss',
+    dest: 'build/assets/styles'
+  },
+  images: {
+    src: 'src/images/**/*.*',
+    dest: 'build/assets/images/'
+  },
+  scripts: {
+    src: 'src/scripts/**/*.js',
+    dest: 'build/assets/scripts/'
   }
 }
 
 //pug
 function templates() {
-  return gulp.src(paths.templates.pages)
+  return gulp
+    .src(paths.templates.pages)
     .pipe(pug({pretty: true}))
     .pipe(gulp.dest(paths.root));
 }
 
-exports.templates = templates
+//scss
+function styles() {
+  return gulp
+    .src('./src/styles/app.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(sourcemaps.write())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest(paths.styles.dest))
+}
+
+//clean
+function clean() {
+  return del(paths.root);
+}
+
+//browser-sync, gulp вотчер
+function watch() {
+  gulp.watch(paths.styles.src, styles);
+  gulp.watch(paths.templates.src, templates);
+  gulp.watch(paths.images.src, images);
+  gulp.watch(paths.scripts.src, scripts);
+}
+
+//локальный сервер, встроенный
+function server() {
+  browserSync.init({
+    server: paths.root
+  });
+  browserSync.watch(paths.root + '/**/*.*', browserSync.reload);
+}
+
+//переносим картинки
+function images() {
+  return gulp.src(paths.images.src)
+      .pipe(gulp.dest(paths.images.dest));
+}
+
+//scripts, webpack
+function scripts() {
+  return gulp.src('src/scripts/app.js')
+      .pipe(gulpWebpack(webpackConfig, webpack)) 
+      .pipe(gulp.dest(paths.scripts.dest));
+}
+
+exports.templates = templates;
+exports.styles = styles;
+exports.clean = clean;
+exports.images = images;
+
+gulp.task('default', gulp.series(
+  clean,
+  gulp.parallel(styles, templates, images, scripts),
+  gulp.parallel(watch, server)
+));
