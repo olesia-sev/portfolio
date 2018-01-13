776f39729428f7a7a53dca52d9832b83d675326b
Olesia <olesya0191@gmail.com>
2018-01-14 02:18:29 +0300

    added auth animation, google map+style, full-screen menu, fixed footer

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,13 +3,142 @@ $(function() {
   $('.welcome__auth-btn').on('click', function(e) {
     e.preventDefault();
 
-    $(this).toggleClass('welcome__auth-btn--active');
+    $(this).addClass('welcome__auth-btn--active');
+    $('.welcome__form--front').addClass('welcome__form--front-active');
+    $('.welcome__form--back').addClass('welcome__form--back-active');
+  })
 
-    $('.welcome__form--front').toggleClass('welcome__form--front-active');
+  $('.to_main').on('click', function(e) {
+    e.preventDefault();
 
-    $('.welcome__form--back').toggleClass('welcome__form--back-active');
+    $('.welcome__auth-btn').removeClass('welcome__auth-btn--active');
+    $('.welcome__form--front').removeClass('welcome__form--front-active');
+    $('.welcome__form--back').removeClass('welcome__form--back-active');
   })
 });
+
+//full-screen menu по клику на бургер
+$(function() {
+  $('.hero__burger-menu').on('click', function(e) {
+    e.preventDefault();
+
+    $('.hero__burger-menu').toggleClass("menu-on");
+    $('.full-screen-menu').toggleClass('full-screen-menu__active');
+  })
+});
+
+//full-screen menu появление слева и справа + текст меню
+$(function() {
+  $('.hero__burger-menu').on('click', function(e) {
+    e.preventDefault();
+
+    $('.full-screen-menu__left').toggleClass('animated slideInLeft');
+    $('.full-screen-menu__right').toggleClass('animated slideInRight');
+    $('.full-screen-menu__item').toggleClass('zoomIn');
+
+//close menu on ESC
+    $(document).on('keydown', function(e) {
+      if ( e.keyCode === 27 ) {
+        $('.full-screen-menu').removeClass('full-screen-menu__active');
+        $('.hero__burger-menu').removeClass("menu-on");
+      }
+    });
+  });
+});
+
+//skiils, function on scroll
+$(function() {
+  var skills = $('.skills-row');
+
+  $(window).on('scroll', function(e) {
+    if (document.body.scrollTop > 120 || 
+    document.documentElement.scrollTop > 120){
+      $('[class^="circle"]').addClass('circle-active')
+    }
+  });
+}) 
+
+//google map
+  function initMap() {
+    var center = {lat:  59.9450154, lng: 30.3085512};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 14,
+      center: center,
+      scrollwheel: false,
+      styles: [
+        {
+          "featureType": "landscape",
+          "elementType": "all",
+          "stylers": [
+          {
+            "color": "#f2f2f2"
+          }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "all",
+          "stylers": [
+            {
+             "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "all",
+          "stylers": [
+            {
+              "saturation": -100
+            },
+            {
+              "lightness": 45
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "all",
+          "stylers": [
+            {
+              "visibility": "simplified"
+            }
+          ]
+        },
+        {
+          "featureType": "road.arterial",
+          "elementType": "labels.icon",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "transit",
+          "elementType": "all",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "all",
+          "stylers": [
+            {
+              "color": "#00BFA5"
+            },
+            {
+              "visibility": "on"
+            }
+          ]
+        }
+    ]
+    });
+  };
+
 
 //blur
 $(function() {
